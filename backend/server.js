4afae2ac4c8f4ed94e5abdf9a96b2de6ddfd5459
Olivier Poupier <olivier@maximiz.ca>
2020-03-24 16:04:02 -0400

    resolve merge conflicts

--- backend/server.js
+++ backend/server.js
@@ -3,38 +3,6 @@ var express        = require('express');
 var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-<<<<<<< HEAD
-const process = require('process');
-var port = process.env.PORT || 8080;
-
-process.on('exit', (code) => {
-  console.log('Process beforeExit event with code: ', code);
-});
-// get all data/stuff of the body (POST) parameters
-// parse application/json
-app.use(bodyParser.json());
-
-// parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override'));
-
-// set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public'));
-
-require('./app/routes')(app); // configure our routes
-
-app.listen(port);
-
-console.log('http://localhost:' + port);
-
-// expose app
-exports = module.exports = app;
-=======
 var dbTools = require('./app/tools/db_tools');
 var port = process.env.PORT || 8080;
 
@@ -63,4 +31,3 @@ dbTools.DBConnectMongo()
         exports = module.exports = app;
     })
     .catch(err => console.log('Error: ' + err))
->>>>>>> bfb66dce5ba465033cfe5459b88ef43b8bbbd027
